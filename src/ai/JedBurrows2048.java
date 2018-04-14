ad093bd1b20dfddaf7842c6b490d8ff4712cd453
Jed Burrows <mjb15164@uni.strath.ac.uk>
2018-04-14 21:17:36 +0100

    added files

---
+++ src/ai/JedBurrows2048.java
@@ -0,0 +1,22 @@
+package ai;
+
+import model.AbstractState;
+import model.State;
+
+import java.util.List;
+
+public class JedBurrows2048 extends AbstractPlayer {
+    @Override
+    public AbstractState.MOVE getMove(State game) {
+    }
+
+    @Override
+    public int studentID() {
+        return 201502941;
+    }
+
+    @Override
+    public String studentName() {
+        return "Jed Burrows";
+    }
+}
