3c7e06aef0642a93331265a6e763dce35f3fe8cf
Jed <mjb15164@uni.strath.ac.uk>
2018-04-15 17:53:25 +0100

    added basic algorithm

--- src/ai/JedBurrows2048.java
+++ src/ai/JedBurrows2048.java
@@ -6,9 +6,59 @@ import model.State;
 import java.util.List;
 
 public class JedBurrows2048 extends AbstractPlayer {
+    private int alpha;
+    private int beta;
+    private int value;
     @Override
     public AbstractState.MOVE getMove(State game) {
-        return null;
+        pause();
+
+        List<AbstractState.MOVE> moves = game.getMoves();
+        maxValue(game);
+        AbstractState.MOVE bestMove = null;
+        return bestMove;
+    }
+
+    private int maxValue(State game) {
+        int v;
+        if(game.getNumberOfEmptyCells() == 0){
+            return 0;
+        }
+        value = Integer.MIN_VALUE;
+        for(State s : game.nextFirstHalfMoveStates()){
+            v = minValue(s);
+            if(v > value){
+                value = v;
+            }
+            if(v >= beta){
+                return value;
+            }
+            if(v > alpha){
+                alpha = v;
+            }
+        }
+        return value;
+    }
+
+    private int minValue(State game){
+        int v;
+        if(game.getNumberOfEmptyCells() == 0){
+            return 0;
+        }
+        value = Integer.MAX_VALUE;
+        for(State s : game.nextFirstHalfMoveStates()){
+            v = maxValue(s);
+            if(v < value){
+                value = v;
+            }
+            if(v <= alpha){
+                return value;
+            }
+            if(v < beta){
+                beta = v;
+            }
+        }
+        return value;
     }
 
     @Override
