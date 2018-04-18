e0fd6013406f718aa49a094cdb3a27c59690e4e2
Jed Burrows <mjb15164@uni.strath.ac.uk>
2018-04-18 18:52:18 +0100

    added methods

--- src/ai/JedBurrows2048.java
+++ src/ai/JedBurrows2048.java
@@ -6,20 +6,18 @@ import java.util.List;
 import java.util.Random;
 
 public class JedBurrows2048 extends AbstractPlayer {
-    private int depth;
-    private int iterations;
     private double bestScore;
     private AbstractState.MOVE bestMove;
 
     @Override
     public AbstractState.MOVE getMove(State game) {
         pause();
-        iterations = 300;
-        depth = 200;
+        int iterations = 300;
+        int depth = 200;
         bestScore = Double.MIN_VALUE;
         List<AbstractState.MOVE> moves = game.getMoves();
         for (AbstractState.MOVE move : moves){
-            double score = findBestMove(move, game);
+            double score = findBestMove(move, game, iterations, depth);
             if(bestScore < score){
                 bestScore = score;
                 bestMove = move;
@@ -28,23 +26,27 @@ public class JedBurrows2048 extends AbstractPlayer {
         return bestMove;
     }
 
-    private double findBestMove(AbstractState.MOVE move, State start) {
+    private double findBestMove(AbstractState.MOVE move, State start,int iterations, int depth) {
         int score = 0;
         for(int i = 0; i < iterations; i++){
             State copy = start.copy();
             copy.move(move);
-            for(int ii=0; ii < depth && !copy.getMoves().isEmpty(); ii++){
+            for(int ii=0; ii < depth && !isTerminal(copy); ii++){
                 List<AbstractState.MOVE> moves = copy.getMoves();
                 AbstractState.MOVE bestNextMove = moves.get(new Random().nextInt(moves.size()));
                 copy.move(bestNextMove);
             }
             score += copy.getScore();
         }
-        return (calcAvgScore(score));
+        return (calcAvgScore(score, iterations));
     }
 
-    private double calcAvgScore(double score){
+    private double calcAvgScore(double score, int iterations){
         return score/iterations;
+    }
+
+    private boolean isTerminal(State state){
+        return state.getMoves().isEmpty();
     }
 
     @Override
